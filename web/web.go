a35426fa8d164d657e6b62bd962ca0e2f407dd13
Paul Stern <paulstern45@gmail.com>
2024-06-21 09:59:02 +0300

    ADREG-1 Draft. Basic backend pagination

--- web/web.go
+++ web/web.go
@@ -2,18 +2,50 @@ package web
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/labstack/echo/v4"
 	"github.com/paul-stern/admission-registry-web/model"
 	"github.com/paul-stern/admission-registry-web/templates"
 )
 
-type Params map[string]string
+var entries model.Entries
+
+func init() {
+	entries = model.GenEntries(1000)
+}
 
 func ShowJournal(c echo.Context) error {
-	l := c.Logger()
-	l.Print("Logger started")
-	wp := templates.NewWebPage("Журнал", model.GenEntries(100), c)
+	// l := c.Logger()
+	// l.Print("Logger started")
+	es, err := Page(c, entries, 100)
+	if err != nil {
+		return err
+	}
+	wp := templates.NewWebPage("Журнал", es, c)
 	c.Echo().Renderer = &wp.Template
 	return c.Render(http.StatusOK, wp.Name(), wp)
+}
+
+func Page(c echo.Context, es model.Entries, n int) (model.Entries, error) {
+	var err error
+	var p int
+	ps := c.QueryParam("p")
+	if ps == "" {
+		p = 1
+	} else {
+		p, err = strconv.Atoi(ps)
+		if err != nil {
+			return model.Entries{}, err
+		}
+
+	}
+	// Define start and end of the Entries slice
+	s := (p - 1) * n
+	e := s + 100
+	return es[s:e], err
+}
+
+func LastPage(c echo.Context, es model.Entries, n int) int {
+	return len(es) % n
 }
